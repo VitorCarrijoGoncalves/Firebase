5c405f012fc35fce9e2a59f3432027498d94390b
raonedaniel <vitorcg002@gmail.com>
2019-04-26 21:56:46 -0300

    Menu implementado

--- app/src/main/java/senai/mobile/com/br/firebase/PrincipalActivity.java
+++ app/src/main/java/senai/mobile/com/br/firebase/PrincipalActivity.java
@@ -1,13 +1,61 @@
 package senai.mobile.com.br.firebase;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class PrincipalActivity extends AppCompatActivity {
+
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        autenticacao = FirebaseAuth.getInstance();
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_admin, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.action_add_usuario) {
+            abrirTelaCadastroUsuario();
+        } else if (R.id.action_sair_admin) {
+            deslogarUsuario();
+        }
+
+        return super.onOptionsItemSelected(item);
+
+    }
+    private void abrirTelaCadastroUsuario() {
+        Intent intent = new Intent(PrincipalActivity.this, CadastroUsuario.class);
+        startActivity(intent);
+    }
+
+    private void deslogarUsuario() {
+
+        autenticacao.signOut();
+
+        Intent intent = new Intent(PrincipalActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+
+    }
+
+
+
 }
